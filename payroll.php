d61cf6e4896ab85d66f4299041695b5547a74a47
edimdendidiwangga <edimdendy@gmail.com>
2017-01-25 18:06:30 +0700

    apa aja

--- payroll.php
+++ payroll.php
@@ -140,8 +140,7 @@ if (!isset($_SESSION['id_bu']) ) {
 									<div class="box-body">
 										<div class="tabbable header-tabs">
 										  <ul class="nav nav-tabs">
-											 
-											 <li id="boxs-periode"><a href="#box_tab2" data-toggle="tab"><i class="fa fa-laptop"></i> <span class="hidden-inline-mobile">Data Gaji Per Periode</span></a></li>
+											 <li id="boxs-periode"><a href="#box_tab2" data-toggle="tab"><i class="fa fa-laptop"></i> <span class="hidden-inline-mobile span-periode">Data Gaji Per Periode</span></a></li>
 											 <li class="active" id="boxs-terbaru"><a href="#box_tab1" data-toggle="tab"><i class="fa fa-laptop"></i> <span class="hidden-inline-mobile">Data Gaji Terbaru</span></a></li>
 										  </ul>
 								<div class="tab-content">
@@ -198,7 +197,6 @@ if (!isset($_SESSION['id_bu']) ) {
 											$no=1;
 											while ($data = mysql_fetch_array($query_tampil)) {
 											?>
-											
 												<tr class="gradeX" id="karyawan_<?php echo $data['id_karyawan']; ?>">
 													<td><?php echo $no; ?></td>
 													<td><?php echo $data['nama_karyawan']; ?></td>
@@ -228,7 +226,7 @@ if (!isset($_SESSION['id_bu']) ) {
 													<td><?php echo $data['bpjs_ketenagakerjaan']; ?></td>
 													<td><?php echo $data['bpjs_kesehatan']; ?></td>
 													<td><?php echo $data['periode_gaji']; ?></td>
-													<td><button id="setting" data-target="#open" data-toggle="modal" data-id="<?php echo $data['id_karyawan']; ?>" data-nama="<?php echo $data['nama_karyawan']; ?>" data-nik="<?php echo $data['nik']; ?>" class="btn btn-info btn-sm"><i class="fa fa-gear"></i></button></td>
+													<td><button id="setting" data-target="#open" data-toggle="modal" data-id="<?php echo $data['id_karyawan']; ?>" data-nama="<?php echo $data['nama_karyawan']; ?>" data-nik="<?php echo $data['nik']; ?>" data-kehadiran="<?php echo $data['kehadiran']; ?>" data-ump="<?php echo $data['ump']; ?>" data-gapok="<?php echo $data['gaji_pokok']; ?>" data-maintenance="<?php echo $data['tun_maintenance']; ?>" data-jabatan="<?php echo $data['tun_jabatan']; ?>" data-jalam="<?php echo $data['tun_jaga_malam']; ?>" data-lain="<?php echo $data['tun_lain']; ?>" data-insentive="<?php echo $data['insentive']; ?>" data-overtime="<?php echo $data['overtime']; ?>" data-rapel="<?php echo $data['rapel']; ?>" data-periode="<?php echo $data['periode_gaji']; ?>" class="btn btn-info btn-sm"><i class="fa fa-gear"></i></button></td>
 												</tr>
 												<?php 
 												$no++;
@@ -275,7 +273,7 @@ if (!isset($_SESSION['id_bu']) ) {
 									<div class="tab-pane fade box-periode" id="box_tab2">
 									
 									</div>
-									<div class="tab-pane fade box-result" id="box_tab2">
+									<div class="tab-pane fade box-result" id="box_tab3">
 									
 									</div>
 									</div>
@@ -313,10 +311,10 @@ if (!isset($_SESSION['id_bu']) ) {
 												   <div class="form-group">
 													<label class="col-sm-4 control-label">Nama Karyawan</label>
 													<div class="col-sm-5">
-													  <input type="text" id="nama_karyawan" class="form-control" disabled>
+													  <input type="text" class="form-control nama" disabled>
 													</div>
 													<div class="col-sm-3">
-													  <input type="text" id="enik" class="form-control" disabled>
+													  <input type="text" class="form-control enik" disabled>
 													</div>
 												  </div>
 												  <div class="form-group">
@@ -405,7 +403,99 @@ if (!isset($_SESSION['id_bu']) ) {
 												  </div>
 								</div>
 								<div class="form-horizontal form_edit_gaji" style="display: none;">
-												  
+								<div class="form-group">
+													<label class="col-sm-4 control-label">Nama Karyawan</label>
+													<div class="col-sm-5">
+													  <input type="text" class="form-control nama" disabled>
+													</div>
+													<div class="col-sm-3">
+													  <input type="text" class="form-control enik" disabled>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Hari Kerja</label>
+													<div class="col-sm-5">
+													  <input type="text" class="form-control" id="kehadiran_ed" name="kehadiran" placeholder="Isikan hari kerja" required>
+													  <span class="error_kehadiran" style="opacity:1; color: red; display: none;">Hari Kerja Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">UMP <?php echo date("Y");?></label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="ump_ed" name="ump" placeholder="Isikan UMP Terbaru" required>
+													  <span class="error_ump" style="opacity:1; color: red; display: none;">UMP Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												   <div class="form-group">
+													<label class="col-sm-4 control-label">Gaji Pokok</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="gaji_pokok_ed" name="gaji_pokok" placeholder="Rp" required>
+													  <span class="error_gaji_pokok" style="opacity:1; color: red; display: none;">Gaji Pokok Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Tunjangan Maintenance</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="tun_maintenance_ed" name="tun_maintenance" placeholder="Rp" required>
+													  <span class="error_tun_maintenance" style="opacity:1; color: red; display: none;">Tunjangan Maintenance Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Tunjangan Jabatan</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="tun_jabatan_ed" name="tun_jabatan" placeholder="Rp" required>
+													  <span class="error_tun_jabatan" style="opacity:1; color: red; display: none;">Tunjangan Jabatan Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Tunjangan Jaga Malam</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="tun_jaga_malam_ed" name="tun_jaga_malam" placeholder="Rp" required>
+													  <span class="error_tun_jaga_malam" style="opacity:1; color: red; display: none;">Tunjangan Jaga Malam Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Tunjangan Lain</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="tun_lain_ed" name="tun_lain" placeholder="Rp" required>
+													  <span class="error_tun_lain" style="opacity:1; color: red; display: none;">Tunjangan Lain Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label"> Insentive</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="insentive_ed" name="insentive" placeholder="Rp" required>
+													   <span class="error_insentive" style="opacity:1; color: red; display: none;">Insentive Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label">Overtime </label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="overtime_ed" name="overtime" placeholder="Rp" required>
+													  <span class="error_overtime" style="opacity:1; color: red; display: none;">Overtime Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label"> Rapel</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" id="rapel_ed" name="rapel" placeholder="Rp" required>
+													  <span class="error_rapel" style="opacity:1; color: red; display: none;">Rapel Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label"> Periode Gaji</label>
+													<div class="col-sm-8">
+													  <input type="text" class="form-control" name="periode_gaji" id="periode_gaji_ed" placeholder="Isikan Periode Gaji" required>
+													  <span class="error_periode_gaji" style="opacity:1; color: red; display: none;">Periode Gaji Tidak Boleh Kosong!</span>
+													</div>
+												  </div>
+												  <div class="form-group">
+													<label class="col-sm-4 control-label"></label>
+													<div class="col-sm-8">
+													<input type="hidden" id="input-gaj-id">
+													<button type="submit" id="sbt_edit_gaji" class="btn btn-success">Simpan</button>
+													</div>
+												  </div>				  
 										</div>
 								</div>
 								<div class="modal-footer">
@@ -501,25 +591,104 @@ if (!isset($_SESSION['id_bu']) ) {
 		rapel : rapel_val,
 		periode_gaji : periode_gaji_val
 		};
-    if(kehadiran == null || kehadiran == '' || kehadiran == undefined) {
+    if(kehadiran_val == null || kehadiran_val == '' || kehadiran_val == undefined) {
        $(".error_kehadiran").fadeIn();
        return false;
-    }if(ump == null || ump == '' || ump == undefined) {
+    }if(ump_val == null || ump_val == '' || ump_val == undefined) {
        $(".error_ump").fadeIn();
        return false;
-    }if(gaji_pokok == null || gaji_pokok == '' || gaji_pokok == undefined) {
+    }if(gaji_pokok_val == null || gaji_pokok_val == '' || gaji_pokok_val == undefined) {
        $(".error_gaji_pokok").fadeIn();
        return false;
-    }if(tun_maintenance == null || tun_maintenance == '' || tun_maintenance == undefined) {
+    }if(tun_maintenance_val == null || tun_maintenance_val == '' || tun_maintenance_val == undefined) {
        $(".error_tun_maintenance").fadeIn();
        return false;
-    }if(tun_jabatan == null || tun_jabatan == '' || tun_jabatan == undefined) {
+    }if(tun_jabatan_val == null || tun_jabatan_val == '' || tun_jabatan_val == undefined) {
        $(".error_tun_jabatan").fadeIn();
        return false;
-    }if(tun_jaga_malam == null || tun_jaga_malam == '' || tun_jaga_malam == undefined) {
+    }if(tun_jaga_malam_val == null || tun_jaga_malam_val == '' || tun_jaga_malam_val == undefined) {
        $(".error_tun_jaga_malam").fadeIn();
        return false;
-    }if(tun_lain == null || tun_lain == '' || tun_lain == undefined) {
+    }if(tun_lain_val == null || tun_lain_val == '' || tun_lain_val == undefined) {
+       $(".error_tun_lain").fadeIn();
+       return false;
+    }if(insentive_val == null || insentive_val == '' || insentive_val == undefined) {
+       $(".error_insentive").fadeIn();
+       return false;
+    }if(overtime_val == null || overtime_val == '' || overtime_val == undefined) {
+       $(".error_overtime").fadeIn();
+       return false;
+    }if(rapel_val == null || rapel_val == '' || rapel_val == undefined) {
+       $(".error_rapel").fadeIn();
+       return false;
+    }if(periode_gaji_val == null || periode_gaji_val == '' || periode_gaji_val == undefined) {
+       $(".error_periode_gaji").fadeIn();
+       return false;
+    }else{
+    	$.ajax({
+    	type: "POST",
+        url: "add-gaji.php",
+        data: data_add_gaji,
+        cache: false,
+        success: function(msg){
+            if(msg == '0'){
+        		alert("Gagal Menambah Data Gaji!");
+        	}else{
+        		$('#content_gaji').html(msg);
+	            $('#open').modal('toggle');
+       		}
+        }
+    	});
+    }
+    
+   });
+
+  $("#sbt_edit_gaji").click(function(){
+  	var id_kyn = $('#input-gaj-id').val();
+    var kehadiran_val = $('#kehadiran_ed').val();
+  	var ump_val = $('#ump_ed').val();
+  	var gaji_pokok_val = $('#gaji_pokok_ed').val();
+  	var tun_maintenance_val = $('#tun_maintenance_ed').val();
+  	var tun_jabatan_val = $('#tun_jabatan_ed').val();
+  	var tun_jaga_malam_val = $('#tun_jaga_malam_ed').val();
+  	var tun_lain_val = $('#tun_lain_ed').val();
+  	var insentive_val = $('#insentive_ed').val();
+  	var overtime_val = $('#overtime_ed').val();
+  	var rapel_val = $('#rapel_ed').val();
+  	var periode_gaji_val = $('#periode_gaji_ed').val();
+  	var data_edit_gaji = {
+		id_karyawan : id_kyn, 
+		kehadiran : kehadiran_val,
+		ump :ump_val,
+		gaji_pokok : gaji_pokok_val, 
+		tun_maintenance : tun_maintenance_val,
+		tun_jabatan : tun_jabatan_val,
+		tun_jaga_malam : tun_jaga_malam_val,
+		tun_lain : tun_lain_val,
+		insentive : insentive_val,
+		overtime : overtime_val,
+		rapel : rapel_val,
+		periode_gaji : periode_gaji_val
+		};
+    if(kehadiran_val == null || kehadiran_val == '' || kehadiran_val == undefined) {
+       $(".error_kehadiran").fadeIn();
+       return false;
+    }if(ump_val == null || ump_val == '' || ump_val == undefined) {
+       $(".error_ump").fadeIn();
+       return false;
+    }if(gaji_pokok_val == null || gaji_pokok_val == '' || gaji_pokok_val == undefined) {
+       $(".error_gaji_pokok").fadeIn();
+       return false;
+    }if(tun_maintenance_val == null || tun_maintenance_val == '' || tun_maintenance_val == undefined) {
+       $(".error_tun_maintenance").fadeIn();
+       return false;
+    }if(tun_jabatan_val == null || tun_jabatan_val == '' || tun_jabatan_val == undefined) {
+       $(".error_tun_jabatan").fadeIn();
+       return false;
+    }if(tun_jaga_malam_val == null || tun_jaga_malam_val == '' || tun_jaga_malam_val == undefined) {
+       $(".error_tun_jaga_malam").fadeIn();
+       return false;
+    }if(tun_lain_val == null || tun_lain_val == '' || tun_lain_val == undefined) {
        $(".error_tun_lain").fadeIn();
        return false;
     }if(insentive == null || insentive == '' || insentive == undefined) {
@@ -537,8 +706,8 @@ if (!isset($_SESSION['id_bu']) ) {
     }else{
     	$.ajax({
     	type: "POST",
-        url: "add-gaji.php",
-        data: data_add_gaji,
+        url: "update_gaji.php",
+        data: data_edit_gaji,
         cache: false,
         success: function(msg){
             if(msg == '0'){
@@ -576,26 +745,47 @@ if (!isset($_SESSION['id_bu']) ) {
             rightColumns: 1
         }
     });
-  
+
 $(document).on("click", "#setting", function () {
      var id_karyawan = $(this).data('id');
      var nama_karyawan = $(this).data('nama');
      var nik = $(this).data('nik');
+     var kehadiran = $(this).data('kehadiran');
+     var ump = $(this).data('ump');
+     var gapok = $(this).data('gapok');
+     var maintenance = $(this).data('maintenance');
+     var jabatan = $(this).data('jabatan');
+     var jalam = $(this).data('jalam');
+     var lain = $(this).data('lain');
+     var insentive = $(this).data('insentive');
+     var overtime = $(this).data('overtime');
+     var rapel = $(this).data('rapel');
+     var periode = $(this).data('periode');
+    $('#input-gaj-id').attr( "value", id_karyawan );
+    $('#kehadiran_ed').val(kehadiran);
+  	$('#ump_ed').val(ump);
+  	$('#gaji_pokok_ed').val(gapok);
+  	$('#tun_maintenance_ed').val(maintenance);
+  	$('#tun_jabatan_ed').val(jabatan);
+  	$('#tun_jaga_malam_ed').val(jalam);
+  	$('#tun_lain_ed').val(lain);
+  	$('#insentive_ed').val(insentive);
+  	$('#overtime_ed').val(overtime);
+  	$('#rapel_ed').val(rapel);
+  	$('#periode_gaji_ed').val(periode);
      $("#id_karyawan").html( id_karyawan );
-     $("#enik").val( nik );
-     $("#nama_karyawan").val( nama_karyawan);
+     $(".enik").val( nik );
+     $(".nama").val( nama_karyawan);
      $("#input-res-id").attr( "value", id_karyawan );
     /* $("#btn-edit").attr( "href", "edit-step.php?id_karyawan="+id_karyawan );
      $("#btn-hapus").attr( "data-id", id_karyawan );
      */
 });
 
-            $( "#periode_gajdi" ).datepicker({
-      showButtonPanel: true,
+            $( "#periode_gaji_ed" ).datepicker({
       changeMonth: true,
       changeYear: true,
-      yearRange: '1950:2026',
-      dateFormat: "M-yy"
+      dateFormat: "MM yy"
     });
           $( "#periode_gaji" ).datepicker({
       changeMonth: true,
